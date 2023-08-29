ab90209017328a5086519f7a7cffc61bfbcf8658
mohmad3308 <mohmadmajadly8@gmail.com>
2023-08-30 00:35:03 +0300

    make a rael informatin about barbers

--- src/main/java/com/server/app/model/barber.java
+++ src/main/java/com/server/app/model/barber.java
@@ -2,6 +2,11 @@ package com.server.app.model;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
+
 
 @Document(collection = "barbers")
 public class barber {
@@ -10,6 +15,23 @@ public class barber {
     private String id;
 
     private String name;
+    private String number;
+    private String email;
+    private String username;
+    private String password;
+    private String firstname;
+    private String lastname;
+    private List<Map<String,List<Map<String,String>>>> times;
+    private String openTime;
+    private String closeTime;
+    private List<String> breakDays;
+    private List<String> hardDates;
+    private String location;
+    private String googleDriveImigUrl;
+    private boolean normalPlan;
+    private boolean reservationPlan;
+    private boolean analysis;
+    private String planEndDate;
     private int quantity;
     private String category;
 
